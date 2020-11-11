46b1bd773e4a8bc05fda3f417b83975a55ad03c7
ytuik <ytuik1@hotmail.com>
2020-11-11 20:55:16 -0300

    damage function inplemented

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var context = canvas.getContext("2d");
 
 function aBirdIsBorn(bird) {
     //.random pra pegar a posição
-    void context.drawImage(bird.image, position.x, position.y, 4, 5);
+    void context.drawImage(bird.image, position.x, position.y,6,5);
     //falar com conti sobre esses atributos de passaro e posicao
     //4 e 5 sao width e heigth da imagem, valores arbitrarios por enquanto
 }
@@ -15,17 +15,32 @@ function aBirdIsDead(phrase, position){
 }
 
 const lilbirdie = document.getElementById("sourceBirdie");
-void context.drawImage(lilbirdie,10,10,47,47);
+context.drawImage(lilbirdie,10,10,30,32);
+
+function luisaHoneyComeHere(freq, bird){
+    let bFreqTop = bird.frequencyTop;
+    let bFreqFloor = bird.frequencyFloor;
+    let bLife = bird.life;
+
+    if (freq <= bFreqTop && freq > bFreqFloor){
+        bird.setLife(bLife - 1);
+    }
+
+    if (bird.life <= 0){
+        aBirdIsDead(bird);
+    }
+
+}
 
 /*
 Próximos Passos
 ----------------
 > Contador de dano (juntar o audio de Marnon e os passaros de Coline e fazer eles interagirem no contador de bruris)
     > Valor da voz chega 
-    > decrementa do limite do passaro 
-    > Se limite do passaro "<= 0" 
-    > chama aBirdIsDead 
-    > matar o passaro 
+    > decrementa do limite do passaro ✅
+    > Se limite do passaro "<= 0" ✅
+    > chama aBirdIsDead ✅
+    > matar o passaro ✅
     > chamar a frase 
     > dar timeout na frase 
     > chama aBirdIsBorn
